83d9ec0ceeb2d4beb5675b3d18b9c7534b0d8afe
CarlosQM <carmanu09@gmail.com>
2021-10-23 15:14:58 -0500

    add nodemon to deoendencies
    Nodemon was added to project modules

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,16 @@ var router = express.Router();
 
 const {getAll} = require('../db/get-all');
 
+//View route
+router.get('/', (req,res)=>{
+  res.render('index',{title:'Students API'})
+})
+
+//CRUD routes
 router.get("/get-all", async(req, res) => {
   const result = await getAll();
-
   res.send(result);
 })
+
 
 module.exports = router;
